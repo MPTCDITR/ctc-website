8fefae4e7bb1279c6592a4aa4dfeb9d35b14174f
Channborey Moul <channnborey.moul@gmail.com>
2024-12-19 09:37:02 +0700

    refactor file of ctc-management

--- src/components/centers-management/centers-list.tsx
+++ src/components/centers-management/centers-list.tsx
@@ -1,21 +1,12 @@
-import { Card } from "@/components/ui/card";
-import { ChevronRight } from "lucide-react";
 import { motion } from "framer-motion";
 import type { CollectionEntry } from "astro:content";
 import TextElement from "@/components/text-element/TextElement";
+import CenterNameCard from "./center-name-card";
 
 interface CentersListProps {
   translations: Record<string, string>;
   centers: CollectionEntry<"centers">[];
   lang: string;
-}
-
-interface CenterCardProps {
-  index: number;
-  name: string;
-  slug: string;
-  lang: string;
-  translations?: Record<string, string>;
 }
 
 export function CenterManagementInfo({
@@ -56,34 +47,5 @@ export function CenterManagementInfo({
         </div>
       </motion.section>
     </div>
-  );
-}
-
-function CenterNameCard({
-  index,
-  name,
-  slug,
-  lang,
-  translations,
-}: CenterCardProps) {
-  const urlSlug = slug.split("/").slice(1).join("/");
-
-  return (
-    <motion.div
-      key={name}
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, delay: 0.8 + index * 0.1 }}
-    >
-      <a href={`/${lang}/center/centers-management/${urlSlug}`}>
-        <Card
-          className={`p-4 h-full text-lg text-primary flex items-center justify-between transition-all duration-300 ease-in-out 
-                hover:scale-105 hover:bg-slate-50 shadow-sm hover:shadow-lightblue hover:border-none`}
-        >
-          <span className="font-semibold">{name}</span>
-          <ChevronRight className="w-5 h-5 text-secondary" />
-        </Card>
-      </a>
-    </motion.div>
   );
 }
