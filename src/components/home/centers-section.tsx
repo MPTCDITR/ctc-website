985bca000941e0ce26c5e984ceeeb2251dfef3a0
lay_vicha <layvicha081783923@gmail.com>
2024-12-24 16:05:17 +0700

    feat: add new national board image in structure and page and restyle location section in home page

--- src/components/home/centers-section.tsx
+++ src/components/home/centers-section.tsx
@@ -1,5 +1,6 @@
 import TextElement from "@/components/text-element/TextElement";
-import PostOfficeView from "@/assets//image-assets/post-office-view.webp";
+import { Button } from "@/components/ui/button";
+import Locations from "@/assets/home/locations.svg";
 
 interface CenterSectionProps {
   translations: Record<string, string>;
@@ -12,19 +13,31 @@ export function CentersSection({ translations, lang }: CenterSectionProps) {
   return (
     <div className="py-16 bg-gray-50">
       <div className="container flex flex-col items-center gap-8">
-        <TextElement variant="heading" className="text-primary">
-          {t("nav.center.ourcenters")}
-        </TextElement>
-        <TextElement variant="body" className="max-w-8xl text-center ">
-          {t("home.center.description")}
-        </TextElement>
-        <div className="w-full max-w-8xl aspect-[16/9] bg-gray-200 rounded-lg">
-          <img
-            src={PostOfficeView.src}
-            alt="c.t.c. post office view"
-            className="w-full"
-            style={{ objectFit: "cover" }}
-          />
+        <div className="flex flex-col md:flex-row gap-5 lg:gap-8">
+          <div>
+            <TextElement variant="heading" className="text-primary">
+              {t("nav.center.ourlocations")}
+            </TextElement>
+            <TextElement variant="body" className="max-w-8xl mt-8">
+              {t("home.center.description")}
+            </TextElement>
+            <Button
+              variant="link"
+              className="w-fit mt-5 lg:text-lg text-secondary hover:text-secondary"
+            >
+              <a href={`/${lang}/center/our-locations`}>
+                {t("home.center.viewalllocations")} →
+              </a>
+            </Button>
+          </div>
+          <div className="w-full max-w-8xl aspect-[16/9] rounded-lg">
+            <img
+              src={Locations.src}
+              alt="18 location of CTC"
+              className="w-full"
+              style={{ objectFit: "cover" }}
+            />
+          </div>
         </div>
       </div>
     </div>
